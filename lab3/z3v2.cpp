3959a8a9534d1438f54a3ad041c32dffed74b735
Aedt <rogowski162@gmail.com>
2022-04-28 01:37:17 +0200

    lab5

---
+++ lab3/z3v2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+    int edge = new int[3][3][1000000];
+    edge[1][2][9754000] = 14;
+    cout << edge[1][2][9754000] << endl;
+
+    
+
+
+
+    return 0;
+}
---
+++ lab5/z5.cpp
@@ -0,0 +1,109 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <string>
+#include <cstring>
+using namespace std;
+
+#define V 10
+
+int minDistance(int dist[], bool sptSet[])
+{
+
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < V; v++)
+        if (sptSet[v] == false && dist[v] <= min)
+            min = dist[v], min_index = v;
+
+    return min_index;
+}
+
+void printSolution(int dist[])
+{
+    cout << "Vertex \t Distance from Source" << endl;
+    for (int i = 0; i < V; i++)
+        cout << i << " \t\t" << dist[i] << endl;
+}
+
+void dijkstra(int **graph, int src)
+{
+    int dist[V];
+    bool sptSet[V];
+    for (int i = 0; i < V; i++)
+        dist[i] = INT_MAX, sptSet[i] = false;
+
+    dist[src] = 0;
+
+    for (int count = 0; count < V - 1; count++)
+    {
+        int u = minDistance(dist, sptSet);
+
+        sptSet[u] = true;
+
+        for (int v = 0; v < V; v++)
+
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
+                dist[v] = dist[u] + graph[u][v];
+    }
+
+    printSolution(dist);
+    
+}
+
+int main()
+{
+    clock_t start, end;
+	double cpu_time;
+
+    int dist[V];
+    int size = 1000;
+    int **graf = new int *[size];
+    for (int i = 0; i < size; ++i)
+    {
+        graf[i] = new int[size];
+    }
+    FILE *file;
+    file = fopen("macierzDodatnie.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("Error reading file\n");
+        exit(1);
+    }
+
+    int newNumber = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            {
+                fscanf(file, "%d,", &newNumber);
+                graf[i][j] = newNumber;
+            }
+        }
+    }
+    fclose(file);
+
+
+    start = clock();
+
+    dijkstra(graf, 0);
+
+    end = clock();
+
+    cpu_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    cout << "exec time: " << cpu_time << endl;
+    for (int i = 0; i < size; ++i)
+    {
+        delete[] graf[i];
+    }
+    delete[] graf;
+
+    return 0;
+}
